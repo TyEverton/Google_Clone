b2b6c55ba227551e6adf72d9db7152711451aed8
Ty Everton <everton_915@yahoo.com>
2021-10-11 19:31:48 -0600

    working on nav bar placement, search bar and title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="navBar">
+        <a className="leftNav">About</a>
+        <a className="leftNav">Store</a>
+        <a className="rightNav">Gmail</a>
+        <a className="rightNav">Images</a>
+      </section>
+      <h1 className="title">Google</h1>
+      <input className="searchBar"></input>
     </div>
-  );
+  )
 }
 
 export default App;
