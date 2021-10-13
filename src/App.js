4257de86ba68cc37c657e941b25b2e872696bdce
Ty Everton <everton_915@yahoo.com>
2021-10-12 18:56:06 -0600

    working on title and searchbar placement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './index.css'
 import SearchBtns from "./SearchBtns"
-import icon from "./SearchIcon.png"
+// import icon from "./SearchIcon.png"
 
 
 
@@ -8,17 +8,19 @@ function App() {
   return (
     <div>
       <section className="navBar">
-        <a className="leftNav">About</a>
-        <a className="leftNav">Store</a>
-        <a className="rightNav">Gmail</a>
-        <a className="rightNav">Images</a>
+        <button className="leftNav">About</button>
+        <button className="leftNav">Store</button>
+        <button className="rightNav">Gmail</button>
+        <button className="rightNav">Images</button>
       </section>
       <h1 className="title">Google</h1>
-      <input className="searchBar"></input>
-      <img className='searchIcon' src={icon}/>
+      <input
+        className="searchBar"
+        // placeholder="Search Google or type a URL"
+      ></input>
+      {/* <img className='searchIcon' src={icon}/> */}
       <SearchBtns />
     </div>
-
   )
 }
 
